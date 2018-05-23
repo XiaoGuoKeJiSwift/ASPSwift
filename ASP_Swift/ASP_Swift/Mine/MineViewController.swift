4eee3f71367b94fbf759e2e0113df30fa60f74b6
赵兵 <bing.zhao@cftcn.com>
2018-05-23 16:04:34 +0800

    tabbar+nav

---
+++ ASP_Swift/ASP_Swift/Mine/MineViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MineViewController.swift
+//  ASP_Swift
+//
+//  Created by 赵兵 on 2018/5/23.
+//  Copyright © 2018年 赵兵. All rights reserved.
+//
+
+import UIKit
+
+class MineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
