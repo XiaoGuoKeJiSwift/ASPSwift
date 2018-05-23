bd15ddd9b11bafaf433104b70b1e37bc6265879e
赵兵 <bing.zhao@cftcn.com>
2018-05-23 13:16:59 +0800

    init

---
+++ ASP_Swift/ASP_Swift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ASP_Swift
+//
+//  Created by 赵兵 on 2018/5/23.
+//  Copyright © 2018年 赵兵. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
